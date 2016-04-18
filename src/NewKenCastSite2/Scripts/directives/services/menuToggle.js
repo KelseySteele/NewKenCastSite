1bdb62407f49e73dd2fbeab4e8a5823000bf322e
kasteele8@gmail.com <kasteele8@gmail.com>
2016-04-18 09:35:36 -0400

    add menuToggle directive

---
+++ src/NewKenCastSite2/Scripts/directives/services/menuToggle.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('kencast')
+        .directive('menuToggle', ['$timeout', function ($timeout) {//Shows & hides menu items when there are sub-items
+            return {
+                scope: {
+                    section: '='//this scope is equal to this section within the sections array
+                },
+
+            }
+
+        }]);
+    
+
+
+})();
---
+++ src/NewKenCastSite2/factories/kencast-services/servicesMenu.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('kencast')
+        .factory('servicesMenu', [
+            '$location', //keeps track of the url in browser
+            '$rootScope', //says this is part of the main model/set of data
+            function ($location) {
+                var sections = [{
+                    name: 'Services',
+                    state: 'services',
+                    type: 'link'
+                }];
+
+                sections.push({
+                    name: 'Fazzt',
+                    type: 'toggle',
+                    pages: [{
+                        name: 'Overview',
+                        type: 'link',
+                        state: 'fazzt.overview',
+                    }, {
+                        name: 'Applications',
+                        type: 'link',
+                        state: 'fazzt.applications',
+                    }, {
+                        name: 'Equipment',
+                        type: 'link',
+                        state: 'fazzt.equipment',
+                    }]
+                });
+            }
+        ]);
+
+
+})();
