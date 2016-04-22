d5cebef2a7cb46d499e5f2377a310c27c4b397c0
Kelsey Steele <kasteele8@gmail.com>
2016-04-22 14:26:55 -0400

    Add first layer of nav bar

--- src/NewKenCastSite2/Scripts/controllers/navigationController.js
+++ src/NewKenCastSite2/Scripts/controllers/navigationController.js
@@ -1,30 +1,29 @@
-﻿//(function () {
-//    'use strict';
+﻿(function () {
+    'use strict';
 
-//    angular
-//        .module('kencast')
-//        .controller('NavigationController', function () {
+    angular
+        .module('kencast')
+        .controller('NavigationController', function () {
 
-//            var menu = this;
+            var menu = this;
 
-//            //menu.toggle = true;
+            //menu.toggle = true;
 
-//            menu.sections = [
-//                {heading:'KenCast', url: '/views/home.html'},
-//                {heading:'Services', url: '/views/services/services.html'},
-//                {heading: 'Case Studies', url: '/views/case-studies/case-studies.html'},
-//                {heading: 'Company', url: '/views/company/company.html'}
-//            ]
+            menu.sections = [
+                {state: 'services', heading:'Services'},
+                {state: 'case-studies', heading: 'Case Studies'},
+                {state: 'company', heading: 'Company'}
+            ]
 
-//            //menu.addSection = function () {
-//            //    menu.sections.push({ heading: sections.heading });
-//            //    menu.heading = "";
+            //menu.addSection = function () {
+            //    menu.sections.push({ heading: sections.heading });
+            //    menu.heading = "";
 
-//            //};
+            //};
 
-//        });
+        });
 
-//})();
+})();
 
 
 
